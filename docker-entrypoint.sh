5427304c83ac698b29a91588c08f1bec9ebe7211
chunqizhi <1558763837@qq.com>
2020-10-10 06:02:53 +0000

    Support gst prefix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 geth --datadir /data init /genesis/genesis.json
 
-geth --datadir /data --networkid 919 --ws --wsaddr 0.0.0.0 --rpc --rpcaddr 0.0.0.0 --rpcapi eth,web3,net
+geth --datadir /data --networkid 919 --ws --wsaddr 0.0.0.0 --rpc --rpcaddr 0.0.0.0 --allow-insecure-unlock --rpccorsdomain "*" --rpcapi eth,web3,net
 
